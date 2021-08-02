5f136bd3937067ab472642d86e816093b94ec3d2
Sahithi Paladi <sahithi.p@tvsnext.io>
2021-08-02 19:57:22 +0530

    Retriving data using where

--- Payroll/PayRollservice.sql
+++ Payroll/PayRollservice.sql
@@ -13,3 +13,7 @@ insert into employee_payroll values('sahithi',24995,'2000-11-13'),('Ashwin',8883
 insert into employee_payroll (name,startDate,salary) values('Sivaangi' ,'2003-03-22',34563)
 --------------UC4---------------
 select * from employee_payroll;
+--------------UC5---------------
+insert into employee_payroll values('Bill',38383,'2018-01-01'),('Mark',56544,'2020-08-09')
+select salary from employee_payroll where name='Bill'
+select * from employee_payroll where startDate between CAST('2018-01-01' As date) and getdate()
