f06b9cc4a58b814566a9d9ba2682676abf9b8e43
Sahithi Paladi <sahithi.p@tvsnext.io>
2021-08-02 19:47:49 +0530

    Inserting data

--- Payroll/PayRollservice.sql
+++ Payroll/PayRollservice.sql
@@ -8,3 +8,6 @@ name varchar(200) not null,
 salary float,
 startDate date
 );
+--------------UC3---------------
+insert into employee_payroll values('Harini',24995,'2000-11-13'),('Arjun',88838,'2002-12-27')
+insert into employee_payroll (name,startDate,salary) values('Kundana' ,'2003-03-22',34563)
