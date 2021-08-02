1bae80f63b3fb778891cb0027d96cc0e0f5bbb83
Sahithi Paladi <sahithi.p@tvsnext.io>
2021-08-02 20:17:42 +0530

    Adding Employee information

--- Payroll/PayRollservice.sql
+++ Payroll/PayRollservice.sql
@@ -28,3 +28,13 @@ select gender,AVG(salary) as averageSalary from employee_payroll group by gender
 select gender,MIN(salary) as totalSalary from employee_payroll group by gender;
 select gender,MAX(salary) as totalSalary from employee_payroll group by gender;
 select gender,COUNT(salary) as countOfPeople from employee_payroll group by gender;
+---------------UC8-------------------
+alter table employee_payroll add phonenumber bigint;
+alter table employee_payroll add address varchar(150) default 'bangalore';
+alter table employee_payroll add department varchar(150) not null default 'HR';
+
+update employee_payroll set phonenumber=8989343478,address='Chennai',department='Sales and Marketing' where name='Kundana';
+update employee_payroll set phonenumber=7833829848,address='Chennai',department='HR' where name='Bill';
+update employee_payroll set phonenumber=8948373829,address='Bangalore',department='HR' where name='Mark';
+update employee_payroll set phonenumber=6748399848,address='Bangalore',department='Sales and Marketing' where name='Arjun';
+update employee_payroll set phonenumber=9494943537,address='Mumbai',department='HR' where name='Harini';
