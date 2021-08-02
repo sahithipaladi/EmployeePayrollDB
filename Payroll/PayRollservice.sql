84180c3139aae06b1d8d876ee3156b6c25a335bb
sahithipaladi <86294777+sahithipaladi@users.noreply.github.com>
2021-08-02 20:28:07 +0530

    Update PayRollservice.sql

--- Payroll/PayRollservice.sql
+++ Payroll/PayRollservice.sql
@@ -21,7 +21,7 @@ select * from employee_payroll where startDate between CAST('2018-01-01' As date
 alter table employee_payroll add gender char(1);
 --Assign Gender to all entries-----
 update employee_payroll  set gender ='M';
-update employee_payroll set gender='F' where name='Ashwin' or name='sahithi';
+update employee_payroll set gender='F' where name='Sivaangi' or name='sahithi';
 --------------UC7---------------
 select gender,SUM(salary) as totalSalary from employee_payroll group by gender;
 select gender,AVG(salary) as averageSalary from employee_payroll group by gender;
@@ -33,11 +33,11 @@ alter table employee_payroll add phonenumber bigint;
 alter table employee_payroll add address varchar(150) default 'bangalore';
 alter table employee_payroll add department varchar(150) not null default 'HR';
 
-update employee_payroll set phonenumber=8989343478,address='Chennai',department='Sales and Marketing' where name='Kundana';
+update employee_payroll set phonenumber=8989343478,address='Chennai',department='Sales and Marketing' where name='Sivaangi';
 update employee_payroll set phonenumber=7833829848,address='Chennai',department='HR' where name='Bill';
 update employee_payroll set phonenumber=8948373829,address='Bangalore',department='HR' where name='Mark';
-update employee_payroll set phonenumber=6748399848,address='Bangalore',department='Sales and Marketing' where name='Arjun';
-update employee_payroll set phonenumber=9494943537,address='Mumbai',department='HR' where name='Harini';
+update employee_payroll set phonenumber=6748399848,address='Bangalore',department='Sales and Marketing' where name='Ashwin';
+update employee_payroll set phonenumber=9494943537,address='Mumbai',department='HR' where name='sahithi';
 
 --------------UC9---------------
 Exec sp_rename 'employee_payroll.salary','basic_pay','COLUMN'
