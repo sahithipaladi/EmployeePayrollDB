7d8ebd70532af442ac02b19bb5d32cc87a2f2019
Sahithi Paladi <sahithi.p@tvsnext.io>
2021-08-02 19:44:56 +0530

    creating a Table

--- Payroll/PayRollservice.sql
+++ Payroll/PayRollservice.sql
@@ -1,2 +1,10 @@
 --------------UC1--------------
 create database payroll_service;
+--------------UC2---------------
+create table employee_payroll
+(
+id int identity(1,1) primary key,
+name varchar(200) not null,
+salary float,
+startDate date
+);
